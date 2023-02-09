59edf4e7a9856226bd257dea6536af76a145462b
alexeyvoytsekhovich <vaiz@inbox.ru>
2023-02-09 00:42:47 -0500

    I am added new class named 3rd class

---
+++ src/day1GigHub/thirdGigHubClass.java
@@ -0,0 +1,11 @@
+package day1GigHub;
+
+public class thirdGigHubClass {
+    //we create own branch under Alex1 to work on it now! we going to make some changes here!
+    public static void main(String[] args) {
+        System.out.println("I am writing this on Alex1 future branch here! ");
+        // master branch not having this class yet!
+        //for some reason master have 3rd class as well ( we did it from checkout on the buttom!)
+
+    }
+}
