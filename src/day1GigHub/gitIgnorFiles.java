b50a35e3371e3ed5e81764e99ab586470a904f96
alexeyvoytsekhovich <vaiz@inbox.ru>
2023-02-09 00:40:49 -0500

    just explanation here!

---
+++ src/day1GigHub/gitIgnorFiles.java
@@ -0,0 +1,14 @@
+package day1GigHub;
+
+public class gitIgnorFiles {
+    // just explanation gitIgnorFile
+    // we no need to send some files to remote repo
+    /*
+    * - .idea
+    * - out
+    * - *.xml
+    * - *.iml
+    * */
+    // we create some file for it on our comp for it! and collect it there!
+    //create new FILE(NOT CLASS) -name it - .gitignore NOT UUNDER SRC ! UNDER PROJECT NAME MAIN ONE!
+}
