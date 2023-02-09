4e694ce0633a11e83769df076704141267f3cdde
alexeyvoytsekhovich <vaiz@inbox.ru>
2023-02-08 23:03:42 -0500

    we just send it to the local repo -maste branch local repo! no changes!  did some changes again! Alex did again some changes ! We make some changes again!

--- src/day1GigHub/secondGitHubClass.java
+++ src/day1GigHub/secondGitHubClass.java
@@ -1,6 +1,8 @@
 package day1GigHub;
 
 public class secondGitHubClass {
+    // first term - ADD =STORE AT THE GIT!
+
     //we not add we canceled -we can not track it its untrack it its out of the git its not at the local mean!
     //if u add it git - ita mean go to local = now its working station -red one
     //
