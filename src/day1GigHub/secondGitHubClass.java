3e94237083dee43484a9085b7a2a8fa22099c95f
alexeyvoytsekhovich <vaiz@inbox.ru>
2023-02-08 22:58:31 -0500

    we just send it to the local repo -maste branch local repo! no changes! for now!

---
+++ src/day1GigHub/secondGitHubClass.java
@@ -0,0 +1,14 @@
+package day1GigHub;
+
+public class secondGitHubClass {
+    //we not add we canceled -we can not track it its untrack it its out of the git its not at the local mean!
+    //if u add it git - ita mean go to local = now its working station -red one
+    //
+    // how i can add to git again?
+    public static void main(String[] args) {
+        System.out.println("Hello B7");
+        //we chalnge to green now add
+        // one more thing !
+        //after add color green!
+    }
+}
