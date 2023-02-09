b51ac04fa5e27e6331605171429575b2d7b07b12
alexeyvoytsekhovich <vaiz@inbox.ru>
2023-02-08 22:56:52 -0500

    i am add some changes

---
+++ src/day1GigHub/firstGihHubClass.java
@@ -0,0 +1,16 @@
+package day1GigHub;
+
+public class firstGihHubClass {
+    public static void main(String[] args) {
+        // main was deleted!
+        // git-add get green!  vcs gone we have git now!
+        //contral sister is git
+        //green -we add to the git
+
+
+        System.out.println("Hello GitHub again!");
+        // we add some code
+        System.out.println(1+1);
+
+    }
+}
